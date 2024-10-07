d932b77f32ff4a85ab72dee62d3aed7330661061
Alexis <180454027+Alexis5631@users.noreply.github.com>
2024-10-07 08:32:17 -0500

    imprimir

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 import os
 import modules.usuarios as us
-
+import modules.utils as ut
+import modules.mensajes as msj
+import modules.menu as mn
+import modules.juego as jg
 
 if __name__ == "__main__":
-    us.crearUsuario()
+    chachipun = []
+    isActive = True
+    opMenu = 0
+    while (isActive):
+        try:
+            os.system('cls')
+            print(msj.tituloPrincipal)
+            print(mn.menuPrincipal)
+            opMenu = int(input(":)_"))
+            match opMenu:
+                case 1:
+                    jugarUnoVsUno = True
+                    while(jugarUnoVsUno):
+                        os.system('cls')
+                        print(msj.tituloPvP)
+                        jg.UnoVsUno()
+                case 2:
+                    jugarUnoVsIa = True
+                    while(jugarUnoVsIa):
+                        os.system('cls')
+                        print(msj.titulo1vsIA)
+                        os.system('pause')
+                case 3:
+                    estadisticas = True
+                    while(estadisticas):
+                        os.system('cls')
+                        print(msj.tituloEstadisticas) 
+                        os.system('pause')                  
+                case 4:
+                    pass
+                case _:
+                    print("La opcion es incorrecta.")
+                    os.system('pause')
+        except ValueError:
+            print("Error en la opcion ingresada")
+            os.system('cls')
+            continue
+        
+        
